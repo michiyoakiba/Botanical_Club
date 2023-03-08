e7bc49b0380e4e6590f3a832cf998653c1a89a63
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-03-08 09:23:21 +0000

    ルーティング作成

---
+++ test/controllers/plants_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class PlantsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get plants_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get plants_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get plants_show_url
+    assert_response :success
+  end
+end
