dc887445c61a3b92e7b77a597164bf46131b2d22
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-03-11 08:27:02 +0000

    views編集中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @plants = @user.plants.page(params[:page])
-    @plants = @user.plants
   end
 
   def edit
