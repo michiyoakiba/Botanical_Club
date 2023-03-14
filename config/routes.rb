aa8dffec817183c40ce1edb92abb1dfbd5f6cecf
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-03-14 16:44:23 +0000

    いいね機能非同期通信化完成

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]  
     resources :plant_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
