c8df1b73818809742db81863c5f0ea1a5d42ba0b
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-03-14 15:55:28 +0000

    いいね機能非同期通信化完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
  before_action :is_matching_login_user, only: [:edit, :update] 
   
+  def index
+  end
+  
   def show
     @user = User.find(params[:id])
     @plants = @user.plants.page(params[:page])
