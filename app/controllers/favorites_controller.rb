e0ba89b1d3c2f377fafe84602d4c0f6c3ba451f5
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-03-14 06:04:48 +0000

    いいね機能非同期通信化作業中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
     
   def create
-    plant = Plant.find(params[:plant_id])
-    favorite = current_user.favorites.new(plant_id: plant.id)
+    @plant = Plant.find(params[:plant_id])
+    favorite = current_user.favorites.new(plant_id: @plant.id) #plant => @plantへ変更
     favorite.save
-    redirect_to plant_path(plant)
+    # redirect_to plant_path(plant)
   end
 
   def destroy
-    plant = Plant.find(params[:plant_id])
-    favorite = current_user.favorites.find_by(plant_id: plant.id)
+    @plant = Plant.find(params[:plant_id])
+    favorite = current_user.favorites.find_by(plant_id: @plant.id) #plant => @plantへ変更
     favorite.destroy
-    redirect_to plant_path(plant)
+    # redirect_to plant_path(plant)
   end
   
 end
