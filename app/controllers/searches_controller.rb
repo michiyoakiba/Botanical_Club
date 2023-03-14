07f2b87e72c582ad044e0b5a2322f7efde7c2dee
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-03-14 11:32:34 +0000

    投稿検索機能（キーワード検索）完成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,14 +1,10 @@
 class SearchesController < ApplicationController
     
   def search
-	@model = params[:model]
-	@content = params[:content]
-	@method = params[:method]
-	if @model == 'user'
-			@records = User.search_for(@content, @method)
-	else
-			@records = Plant.search_for(@content, @method)
+		@model = params[:model]
+		@content = params[:content]
+		@method = params[:method]
+		@records = Plant.search_for(@content, @method).page(params[:page])
 	end
-  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,16 +29,16 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [100, 100]).processed
   end
   
-  def self.search_for(content, method)
-    if method == 'perfect'
-      User.where(name: content)
-    elsif method == 'forward'
-      User.where('name LIKE ?', content + '%')
-    elsif method == 'backward'
-      User.where('name LIKE ?', '%' + content)
-    else
-      User.where('name LIKE ?', '%' + content + '%')
-    end
-  end
+  # def self.search_for(content, method)
+  #   if method == 'perfect'
+  #     User.where(name: content)
+  #   elsif method == 'forward'
+  #     User.where('name LIKE ?', content + '%')
+  #   elsif method == 'backward'
+  #     User.where('name LIKE ?', '%' + content)
+  #   else
+  #     User.where('name LIKE ?', '%' + content + '%')
+  #   end
+  # end
   
 end
