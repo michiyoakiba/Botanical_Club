37f92880120611015e3474277e54fc6637987305
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-03-15 15:24:24 +0000

    フォロー/フォロワー機能完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,30 +7,24 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :plant_comments, dependent: :destroy
   
-  
-   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
   
-  # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  # 与フォロー関係を通じて参照→自分がフォローしている人 (ミル君.マル君)
   has_many :followings, through: :relationships, source: :followed
   
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   
   has_one_attached :profile_image
   
-  def get_profile_image
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [100, 100]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
-  
+
   def follow(user)
     relationships.create(followed_id: user.id)
   end
