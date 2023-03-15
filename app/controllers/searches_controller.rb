8f299eaeccb768ccbfd4b00932759b2a19515e7d
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-03-15 06:01:38 +0000

    投稿検索機能.ユーザ検索機能完成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,21 @@
 class SearchesController < ApplicationController
     
+  #def search
+		# @model = params[:model]
+		# @content = params[:content]
+		# @method = params[:method]
+		# @records = Plant.search_for(@content, @method).page(params[:page])
+  #end
+  
   def search
 		@model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
-		@records = Plant.search_for(@content, @method).page(params[:page])
-	end
+		if @model == 'user'
+			@records = User.search_for(@content, @method)
+		else
+			@records = Plant.search_for(@content, @method).page(params[:page])
+		end
+  end
   
 end
