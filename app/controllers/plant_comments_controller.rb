2d0bf3d0bbb9d9be8acb6514d0d69eac36ef9113
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-03-23 07:31:13 +0000

    [Fix] JS_error

--- app/controllers/plant_comments_controller.rb
+++ app/controllers/plant_comments_controller.rb
@@ -1,18 +1,34 @@
 class PlantCommentsController < ApplicationController
-
-  def create
-    plant = Plant.find(params[:plant_id])
-    comment = current_user.plant_comments.new(plant_comment_params)
-    comment.plant_id = plant.id
-    comment.save
-    redirect_to plant_path(plant)
-  end
   
+  # 部分テンプレートにした箇所(コメント機能非同期通信箇所)
+  # def create
+  #   plant = Plant.find(params[:plant_id])
+  #   comment = current_user.plant_comments.new(plant_comment_params)
+  #   comment.plant_id = plant.id
+  #   comment.save
+  #   redirect_to plant_path(plant)
+  # end
+  
+  # def destroy
+  #   @plant = Plant.find(params[:plant_id])
+  #   plant_comment = @plant.plant_comments.find(params[:id])
+  #   plant_comment.destroy
+  #   redirect_to plant_path(params[:plant_id])
+  # end
+  
+  # コメント機能非同期通信箇所
+  def create
+    @plant = Plant.find(params[:plant_id])
+    comment = current_user.plant_comments.new(plant_comment_params)
+    comment.plant_id = @plant.id
+    comment.save
+    render 'plants/show'
+  end
+
   def destroy
     @plant = Plant.find(params[:plant_id])
-    plant_comment = @plant.plant_comments.find(params[:id])
+    plant_comment = @plant.plant_comments.find_by(params[:id])
     plant_comment.destroy
-    redirect_to plant_path(params[:plant_id])
   end
   
   private
